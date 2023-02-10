d14a4f88beb4f820a3f33a7344c977d7fcca41fd
gravesti <isaac.gravestock@roche.com>
2023-02-10 09:48:59 +0100

    start rotations

---
+++ demo.R
@@ -0,0 +1,13 @@
+
+library(devout)
+library(rgoogleslides)
+
+authorize(
+  client_id = "1073903696751-gr5k1k39e97ufjl5ssklhqp9pv787rfo.apps.googleusercontent.com",
+  client_secret = "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"
+)
+
+
+rdevice(debug_gsd_function, slides_id = "1k38RIlrzaKx2ASie9gzLl6zvZ6bnAGnNz6amY_WcS8w")
+
+plot(1:3)
--- gsd_2.R
+++ gsd_2.R
@@ -1,6 +1,6 @@
 if (!require("devout")) remotes::install_github('coolbutuseless/devout')
+if (!require("rgoogleslides")) remotes::install_github('igrave/rgoogleslides')
 
-library(devout)
 
 new_id <- function(prefix = "ID") {
   paste0(
@@ -18,7 +18,7 @@ gsd_circle <- function(args, state) {
   x2 <- x + r
   y1 <- y - r
   y2 <- y + r
-  # browser()
+
   slide_element <- page_element_property(state$rdata$slidepage_id, x2 - x1, y2 - y1, 1, 1, 0, 0, x1, y1)
   request <- add_create_shape_request(shape_type = "ELLIPSE", page_element_property = slide_element)
   commit_to_slides(state$rdata$slides_id, request)
@@ -41,7 +41,7 @@ gsd_line <- function(args, state) {
   y1 <- args$y1
   x2 <- args$x2
   y2 <- args$y2
-  
+
   if (x1 == x2 && y1 == y2) return()
   # browser()
   height <- abs(y2 - y1)
@@ -61,17 +61,17 @@ gsd_polyline <- function(args, state) {
   x <- args$x
   y <- args$y
   n <- args$n
-  
+
   x = x[1:n]
-  y = y[1:n]   
+  y = y[1:n]
   request <-  NULL
-  
+
   for(i in seq_len(n-1)){
     x1 <- x[i]
     y1 <- y[i]
     x2 <- x[i+1]
     y2 <- y[i+1]
-    
+
     height <- abs(y2 - y1)
     width <- abs(x2 - x1)
     scale_x <- sign(x2 - x1)
@@ -91,15 +91,15 @@ gsd_polygon <- function(args, state) {
   n <- args$n
   x <- c(args$x, args$x[1])
   y <- c(args$y, args$y[1])
-     
+
   request <-  NULL
-  
+
   for(i in seq_len(n)){
     x1 <- x[i]
     y1 <- y[i]
     x2 <- x[i+1]
     y2 <- y[i+1]
-    
+
     height <- abs(y2 - y1)
     width <- abs(x2 - x1)
     scale_x <- sign(x2 - x1)
@@ -118,9 +118,22 @@ gsd_textUTF8 <- function(args, state) {
   x <- args$x
   y <- args$y
   str <- args$str
-  rot <- args$rot
+  rot <- args$rot * pi / 180
   hadj <- args$hadj
-  slide_element <- page_element_property(state$rdata$slidepage_id, 100, 20, 1, 1, 0, 0, x, y)
+
+  print(paste0("rot: ", rot))
+  slide_element <- page_element_property(
+    state$rdata$slidepage_id,
+    width_magnitude = 100,
+    height_magnitude = 20,
+    scale_x = cos(rot),
+    scale_y = cos(rot),
+    shear_x = -sin(rot),
+    shear_y = sin(rot),
+    translate_x = x,
+    translate_y = y
+    )
+
   text_box_id <- new_id("TEXTBOX")
   request <- add_create_shape_request(
     shape_type = "TEXT_BOX",
@@ -139,27 +152,31 @@ gsd_newPage <- function(args, state) {
   request <- add_create_slide_page_request(predefined_layout = "BLANK")
   res <- commit_to_slides(state$rdata$slides_id, request)
   state$rdata$slidepage_id <- res$replies$createSlide$objectId
-  
-  state$dd$left <- 0
-  state$dd$top <- 0
-  state$dd$right <- 720
-  state$dd$bottom <- 300
-  
+
   state
 }
 
 
 gsd_open <- function(args, state) {
-  state$rdata$slides_id <- create_slides("TEXT")
+  if(is.null(state$rdata$slides_id)) {
+    state$rdata$slides_id <- create_slides("gslideDevice")
+  }
+
+  state$dd$left <- 0
+  state$dd$top <- 0
+  state$dd$right <- 720
+  state$dd$bottom <- 400
+  state$dd$canClip <- FALSE
+
   state
 }
 
 
 debug_gsd_function <- function(device_call, args, state) {
   if (device_call %in% c('mode', 'strWidthUTF8', 'metricInfo')) return()
-  cat("[", device_call, "]: ")
-  cat(paste(names(args), args, sep = " = ", collapse = ",  "), "\n", sep = "")
-  
+  cat("[", device_call, "]\n")
+  # cat(paste(names(args), args, sep = " = ", collapse = ",  "), "\n", sep = "")
+
   call_string <- paste(
     "[", device_call, "]: ",
     paste(names(args), args, sep = " = ", collapse = ",  ")
@@ -177,7 +194,7 @@ debug_gsd_function <- function(device_call, args, state) {
       "textUTF8" = gsd_textUTF8(args, state),
       state
       )
-  }, 
+  },
   error = function(e) {
     cat("Error in call:\n")
     cat(call_string, "\n")
@@ -188,16 +205,6 @@ debug_gsd_function <- function(device_call, args, state) {
 }
 
 
-library(rgoogleslides)
-
-authorize(
-  client_id = "1073903696751-gr5k1k39e97ufjl5ssklhqp9pv787rfo.apps.googleusercontent.com",
-  client_secret = "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"
-)
 
 
-
-
-rdevice(debug_gsd_function) 
-plot(1:3)
-invisible(dev.off())
+# invisible(dev.off())
