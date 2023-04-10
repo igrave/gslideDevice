fb4d48ce7cb840e58e0b050dce1564d59779827f
igrave <isaac.gravestock@gmail.com>
2023-04-10 22:12:20 +0200

    skeleton

--- R/hook.R
+++ R/hook.R
@@ -1,14 +1,17 @@
-add_to_slides <- function(before, options, envir) {
-  if (before) return()
-
-  paths <- unique(opts_knit$get("plot_files"))
-
-  for (path in paths) {
-    # get dimensions
-    base64_string <- knitr::image_uri(path)
-    slide_id <- add_slide()
-    add_base64_shape(slide_id, base64_string)
-    add_title(slide_id)
-  }
-
-}
+# add_to_slides_hook <- function(before, options, envir) {
+#   if (before) return()
+#
+#   paths <- unique(opts_knit$get("plot_files"))
+#
+#   for (path in paths) {
+#     # get dimensions
+#     # base64_string <- knitr::image_uri(path)
+#     # slide_id <- add_slide()
+#     # add_base64_shape(slide_id, base64_string)
+#     # add_title(slide_id)
+#   }
+# }
+#
+# convert_image_strings_hook <- function(presentation_id) {
+#   # run_apps_script(presentation_id)
+# }
